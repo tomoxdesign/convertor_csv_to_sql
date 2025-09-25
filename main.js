86001b780173c1857a12a981672ba3ce7fd03fd0
Tomáš Kyncl <tomox.design@gmail.com>
2025-09-25 10:35:30 +0200

    Update package.json and add new icon for Windows packaging
    - Modified the `win-package-build` script to use `npx electron-packager` and updated the icon path.
    - Added `@electron/packager` as a new development dependency.
    - Added a new icon file for the application in the assets/icons directory.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+        icon: path.join(__dirname, 'assets/icons/icon.ico'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
